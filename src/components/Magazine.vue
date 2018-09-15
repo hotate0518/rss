fae12d8c2f70bc7428da42df27e224c18a0b9aaa
you0518 <llllllllllv0@gmail.com>
2018-09-15 13:45:28 +0900

    init

---
+++ src/components/Magazine.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="magazine">
+    <h2>{{title}}</h2>
+    <h3>{{sentence}}</h3>
+    <img :src="thumbnail" />
+  </div>
+</template>
+
+<script>
+import image from '../assets/logo.png'
+
+export default {
+  props: {
+    title : {
+
+    },
+    sentence: {
+
+    }
+  },
+  data () {
+    return {
+      thumbnail: image
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
---
+++ src/views/TopPage.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <Magazine title="title" sentence="sentence"/>
+    <button class="load" @click="getRss()">ロード</button>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+import Magazine from '../components/Magazine.vue'
+import RssList from '../rss/rss.json'
+axios.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
+export default {
+  components: {
+    Magazine
+  }, 
+  methods: {
+    getRss() {
+      console.log(RssList)
+      RssList.forEach(el => {
+        axios.get(el.url)
+        .then(json => {})
+        .catch(err => {})
+      });
+
+    }
+  }
+}
+</script>
+
