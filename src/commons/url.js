30ef6be0977d844b0fe86eea4b4cd26484c55b88
you0518 <llllllllllv0@gmail.com>
2018-09-27 00:30:58 +0900

    urlの設定かえた

--- src/commons/url.js
+++ src/commons/url.js
@@ -1,2 +1,5 @@
-export const getArticles = 'http://localhost:8081/get-articles';
-export const getHomePages = 'http://192.168.11.6:8081/get-homepages';
+export const getArticles = 'https://4jdp5h63t2.execute-api.ap-northeast-1.amazonaws.com/hotateAPI/hotaterss/get-articles';
+export const getHomePages = 'https://4jdp5h63t2.execute-api.ap-northeast-1.amazonaws.com/hotateAPI/hotaterss/get-homepages';
+
+// export const getArticles = 'http://192.168.11.3:8081/get-articles';
+// export const getHomePages = 'http://192.168.11.3:8081/get-homepages';
