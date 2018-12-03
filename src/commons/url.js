1c239632a7fd42f4f5f88edd4ddf1718364f7ead
hotate0518 <45566895+hotate0518@users.noreply.github.com>
2018-12-03 23:58:05 +0900

    delete

--- src/commons/url.js
+++
@@ -1,5 +0,0 @@
-export const getArticles = 'https://4jdp5h63t2.execute-api.ap-northeast-1.amazonaws.com/hotateAPI/hotaterss/get-articles';
-export const getHomePages = 'https://4jdp5h63t2.execute-api.ap-northeast-1.amazonaws.com/hotateAPI/hotaterss/get-homepages';
-
-// export const getArticles = 'http://192.168.11.3:8081/get-articles';
-// export const getHomePages = 'http://192.168.11.3:8081/get-homepages';
